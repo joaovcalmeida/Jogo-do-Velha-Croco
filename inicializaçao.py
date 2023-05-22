b379e723d18a5f738f06079c00955f701b7b338b
joaovcalmeida <joao.v.campos.almeida@gmail.com>
2023-05-22 10:28:45 -0300

    cores da tela inicial

---
+++ inicializaçao.py
@@ -0,0 +1,34 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Jogo da velha')
+
+#Cores
+cor_amarela = (255, 255, 0)
+cor_azul = (0,50,210)
+cor_verde = (35,142,35)
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill(cor_verde)  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
