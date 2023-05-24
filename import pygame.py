be05a73255daa0ef649d52f81620dc8957cd0276
Joao Pedro Brand Guimaraes <joaopbg1@al.insper.edu.br>
2023-05-24 08:44:24 -0300

    import pygame e assets.py completados

--- import pygame.py
+++ import pygame.py
@@ -6,53 +6,60 @@ from config import IMG_DIR, SND_DIR, FNT_DIR
 BACKGROUND = 'background'
 CROCO_IMG = 'croco_img'
 PESSOA_IMG = 'pessoa_img'
+PLAY_INICIAL_IMG='play_inicial_img'
+PLAY_INICIAL_SELECIONADA_IMG = 'play_inicial_selecionada_img'
 CROCO_SOUND = 'croco_sound'
 DJ_SOUND = 'dj_sound'
 RIVER_SOUND ='river_sound'
 WIN_SOUND = 'win_sound'
 
-
-
-
-
 def load_assets():
     assets = {}
-    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR,'')).convert()
-    assets[CROCO_IMG] = pygame.image.load(os.path.join(IMG_DIR, '')).convert_alpha()
-    assets[PESSOA_IMG] = pygame.image.load(os.path.join(IMG_DIR,'')).convert_alpha()
-
+    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR,'6206152.jpg')).convert()
+    assets[CROCO_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'crocodilo jogo da velha.jpg')).convert_alpha()
+    assets[PESSOA_IMG] = pygame.image.load(os.path.join(IMG_DIR,'imagem pessoa 1,jpeg')).convert_alpha()
+    assets[PLAY_INICIAL_IMG] = pygame.image.load(os.path.join(IMG_DIR,'play inicial.png')).convert_alpha()
+    assets[PLAY_INICIAL_SELECIONADA_IMG] = pygame.image.load(os.path.join(IMG_DIR,'play inicial mais clara.png')).convert_alpha()
 # Carrega os sons do jogo
     pygame.mixer.music.load(os.path.join(SND_DIR, ''))
     pygame.mixer.music.set_volume(0.4)
-    assets[DJ_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl3.wav'))
-    assets[RIVER_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl6.wav'))
-    assets[WIN_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'pew.wav'))
+    assets[DJ_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'dj_caionoveli sound.m4a'))
+    assets[RIVER_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'river sound.mp3'))
+    assets[WIN_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'win sound.mp3'))
+    assets[CROCO_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'crocodile sound.mp3'))
     return assets
-
-
 
 # Jogo-do-Velha-Croco
 
-
 # ----- Inicia assets
-CROCODILO_WIDTH = 50
-CROCODILO_HEIGHT = 38
-PESSOA_WIDTH = 50
-PESSOA_HEIGHT = 38
+CROCODILO_WIDTH = 300
+CROCODILO_HEIGHT = 300
+PLAY_INICIAL_WIDTH = 600
+PLAY_INICIAL_HEIGHT = 600
+PLAY_SELECIONADA_WIDTH = 600
+PLAY_SELECIONADA_HEIGHT =600
+PESSOA_WIDTH = 300
+PESSOA_HEIGHT = 300
+BACKGROUND_WIDTH = 800
+BACKGROUND_HEIGHT = 600
 
 assets={}
-assets['background'] = pygame.image.load('assets/img/').convert()
-assets['crocodilo_img'] = pygame.image.load('assets/img/').convert_alpha()
-assets['pessoa_img'] = pygame.transform.scale(assets['pessoa_img'], (METEOR_WIDTH, METEOR_HEIGHT))
-assets['ship_img'] = pygame.image.load('assets/img/playerShip1_orange.png').convert_alpha()
-assets['ship_img'] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-assets['bullet_img'] = pygame.image.load('assets/img/laserRed16.png').convert_alpha()
+assets['background'] = pygame.image.load('assets/img/6206152.jpg').convert()
+assets['pessoa_img'] = pygame.transform.scale(assets['6206152.jpg'], (BACKGROUND_WIDTH, BACKGROUND_HEIGHT))
+assets['crocodilo_img'] = pygame.image.load('assets/img/crocodilo jogo da velha.jpg').convert_alpha()
+assets['crocodilo_img'] = pygame.transform.scale(assets['crocodilo jogo da velha.jpg'], (CROCODILO_WIDTH,CROCODILO_HEIGHT))
+assets['play_inicial_img'] = pygame.image.load('assets/img/play inicial.png').convert_alpha()
+assets['play_inicial_img'] = pygame.transform.scale(assets['play inicial.png'], (PLAY_INICIAL_WIDTH, PLAY_INICIAL_HEIGHT))
+assets['play_inicial_selecionada_img'] = pygame.image.load('assets/img/play inicial mais clara.png').convert_alpha()
+assets['play_inicial_selecionada_img'] = pygame.transform.scale(assets['play inicial mais clara.png'], (PLAY_SELECIONADA_WIDTH , PLAY_SELECIONADA_HEIGHT))
+assets['pessoa_img'] = pygame.image.load('assets/img/imagem pessoa 1.jpeg').convert_alpha()
+assets['pessoa_img'] = pygame.transform.scale(assets['imagem pessoa 1.jpeg'], (PESSOA_WIDTH, PESSOA_HEIGHT))
+
 
 # Carrega os sons do jogo
 pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
 pygame.mixer.music.set_volume(0.4)
-boom_sound = pygame.mixer.Sound('assets/snd/expl3.wav')
-destroy_sound = pygame.mixer.Sound('assets/snd/expl6.wav')
-assets['river_sound'] = pygame.mixer.Sound('assets/snd/pew.wav')
-assets['croco_sound'] = pygame.mixer.Sound('assets/snd/expl3.wav')
-assets['dj_sound'] =  pygame.mixer.Sound('assets/snd/expl6.wav')
+assets['river_sound'] = pygame.mixer.Sound('assets/snd/river sound.mp3')
+assets['croco_sound'] = pygame.mixer.Sound('assets/snd/crocodile sound.mp3')
+assets['dj_sound'] =  pygame.mixer.Sound('assets/snd/dj_caionoveli sound.m4a')
+assets['win_sound'] =  pygame.mixer.Sound('assets/snd/win sound.mp3')
