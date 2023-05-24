004b647034558f9aa29e14d133e717fe7fe29d1e
pedroefnf_arquivos <pedroefnfranca@gmail.com>
2023-05-24 09:49:11 -0300

    game srceen retângulos

--- game_screen.py
+++ game_screen.py
@@ -12,6 +12,13 @@ def game_screen(window):
     PLAYING = 1
     state = PLAYING
 
+    
+    # Initializing Color
+    color = (255,255,255)
+    top = 100
+    left = 100
+
+
     # ===== Loop principal =====
     while state != DONE:
         clock.tick(FPS)
@@ -23,15 +30,60 @@ def game_screen(window):
                 state = DONE
 
         # ----- Gera saídas
-        window.fill(BLACK)  # Preenche com a cor branca
+        window.fill(BACKGROUND)  # Preenche com a cor branca
+
+        r1 = pygame.draw.rect(window, color, pygame.Rect(left, top, 120, 100))
+        pygame.display.flip()
+        r2 = pygame.draw.rect(window, color, pygame.Rect(300, top, 120, 100))
+        pygame.display.flip()
+        r3 = pygame.draw.rect(window, color, pygame.Rect(500, top, 120, 100))
+        pygame.display.flip()
+        r4 = pygame.draw.rect(window, color, pygame.Rect(left, 300, 120, 100))
+        pygame.display.flip()
+        r5 = pygame.draw.rect(window, color, pygame.Rect(300, 300, 120, 100))
+        pygame.display.flip()
+        r6 = pygame.draw.rect(window, color, pygame.Rect(500, 300, 120, 100))
+        pygame.display.flip()
+        r7 = pygame.draw.rect(window, color, pygame.Rect(left, 500, 120, 100))
+        pygame.display.flip()
+        r8 = pygame.draw.rect(window, color, pygame.Rect(300, 500, 120, 100))
+        pygame.display.flip()
+        r9 = pygame.draw.rect(window, color, pygame.Rect(500, 500, 120, 100))
+        pygame.display.flip()
 
 
-        tela_texto = assets['font_media'].render("Tela com o seu jogo", True, WHITE)
-        text_rect = tela_texto.get_rect()
-        text_rect.centerx = LARGURA / 2
-        text_rect.centery = 200
-        window.blit(tela_texto, text_rect)
+        e = 0
+        while e < 9:
+            if e == 0 or e%2 == 0:
+                r1 = CROCO_IMG
+                r2 = CROCO_IMG
+                r3= CROCO_IMG
+                r4= CROCO_IMG
+                r5= CROCO_IMG
+                r6= CROCO_IMG
+                r7= CROCO_IMG
+                r8= CROCO_IMG
+                r9= CROCO_IMG
+            else:
+                r1 = PESSOA_IMG
+                r2= PESSOA_IMG
+                r3= PESSOA_IMG
+                r4= PESSOA_IMG
+                r5= PESSOA_IMG
+                r6= PESSOA_IMG
+                r7= PESSOA_IMG
+                r8= PESSOA_IMG
+                r9= PESSOA_IMG
+
 
         pygame.display.update()  # Mostra o novo frame para o jogador
 
     return state
+
+window = pygame.display.set_mode((LARGURA,ALTURA))
+state = INIT 
+while state != QUIT:
+    if state == GAME:
+        state = game_screen(window)
+    else:
+        state = QUIT
