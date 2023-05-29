ce788d750a67ebf7877954c1b137181265c6eaea
Joao Pedro Brand Guimaraes <joaopbg1@al.insper.edu.br>
2023-05-29 14:15:38 -0300

    change

--- assets.py
+++ assets.py
@@ -20,7 +20,7 @@ def load_assets():
     assets[CROCO_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'crocodilo_jogo_da_velha.jpg')).convert_alpha()
     assets[CROCO_IMG] = pygame.transform.scale(assets[CROCO_IMG], (199, 199))
     assets[PESSOA_IMG] = pygame.image.load(os.path.join(IMG_DIR,'imagem_pessoa1.jpeg')).convert_alpha()
-    assets[PESSOA_IMG] = pygame.transform.scale(assets[PESSOA_IMG], (195, 195))
+    assets[PESSOA_IMG] = pygame.transform.scale(assets[PESSOA_IMG], (199, 199))
     assets[PESSOA_IMG] = pygame.transform.rotate(assets[PESSOA_IMG],270)
     assets[PLAY_INICIAL_IMG] = pygame.image.load(os.path.join(IMG_DIR,'play_inicial.png')).convert_alpha()
     assets[PLAY_INICIAL_IMG] = pygame.transform.scale(assets[PLAY_INICIAL_IMG], (300, 300))
